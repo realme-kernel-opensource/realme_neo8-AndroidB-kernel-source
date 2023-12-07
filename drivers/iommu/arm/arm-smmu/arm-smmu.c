7331c51176ee7909848b55b77815b3847852daac
LADI RAM SAI <quic_lramsai@quicinc.com>
2023-12-07 18:19:13 +0530

    iommu: arm-smmu: Add Deepsleep support for smmu driver
    Add Deepsleep support for arm-smmu driver.
    
    This snapshot is taken as of msm-5.15 branch commit 21da64c0bf5f7
    ("iommu: arm-smmu: Add Deepsleep support for smmu").
    
    In kernel-6.1 we are checking the power state with
    pm_suspend_target_state instead of pm_suspend_via_firmware()
    due to the limitations of GKI constraints with power_state driver.
    
    Change-Id: Ic348d1b713bf9869707305a68ff2dfa5b4551a37
    Signed-off-by: Nagireddy Annem <quic_nannem@quicinc.com>
    Signed-off-by: LADI RAM SAI <quic_lramsai@quicinc.com>
    (cherry picked from commit 4aa803260d2890e51b0ea348cef32182f21766a3)

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -51,6 +51,7 @@
 #include "../../qcom-dma-iommu-generic.h"
 #include "../../qcom-io-pgtable-alloc.h"
 #include <linux/qcom-iommu-util.h>
+#include <linux/suspend.h>
 
 #define CREATE_TRACE_POINTS
 #include "arm-smmu-trace.h"
@@ -3770,7 +3771,7 @@ static int __maybe_unused arm_smmu_runtime_suspend(struct device *dev)
 	return 0;
 }
 
-static int __maybe_unused arm_smmu_pm_resume(struct device *dev)
+static int __maybe_unused arm_smmu_pm_resume_common(struct device *dev)
 {
 	int ret;
 	struct arm_smmu_device *smmu = dev_get_drvdata(dev);
@@ -3796,24 +3797,6 @@ static int __maybe_unused arm_smmu_pm_resume(struct device *dev)
 	arm_smmu_device_reset(smmu);
 	return ret;
 }
-
-static int __maybe_unused arm_smmu_pm_suspend(struct device *dev)
-{
-	int ret = 0;
-	struct arm_smmu_device *smmu = dev_get_drvdata(dev);
-
-	if (pm_runtime_suspended(dev))
-		goto clk_unprepare;
-
-	ret = arm_smmu_runtime_suspend(dev);
-	if (ret)
-		return ret;
-
-clk_unprepare:
-	clk_bulk_unprepare(smmu->num_clks, smmu->clks);
-	return ret;
-}
-
 
 static int arm_smmu_pm_prepare(struct device *dev)
 {
@@ -3865,7 +3848,7 @@ static int __maybe_unused arm_smmu_pm_restore_early(struct device *dev)
 		smmu_domain->pgtbl_ops = pgtbl_ops;
 		arm_smmu_init_context_bank(smmu_domain, pgtbl_cfg);
 	}
-	arm_smmu_pm_resume(dev);
+	arm_smmu_pm_resume_common(dev);
 	ret = arm_smmu_runtime_suspend(dev);
 	if (ret) {
 		dev_err(dev, "Failed to suspend\n");
@@ -3904,6 +3887,34 @@ static int __maybe_unused arm_smmu_pm_freeze_late(struct device *dev)
 	}
 	arm_smmu_power_off(smmu, smmu->pwr);
 	return 0;
+}
+
+static int __maybe_unused arm_smmu_pm_suspend(struct device *dev)
+{
+	int ret = 0;
+	struct arm_smmu_device *smmu = dev_get_drvdata(dev);
+
+	if (pm_suspend_target_state == PM_SUSPEND_MEM)
+		return arm_smmu_pm_freeze_late(dev);
+
+	if (pm_runtime_suspended(dev))
+		goto clk_unprepare;
+
+	ret = arm_smmu_runtime_suspend(dev);
+	if (ret)
+		return ret;
+
+clk_unprepare:
+	clk_bulk_unprepare(smmu->num_clks, smmu->clks);
+	return ret;
+}
+
+static int __maybe_unused arm_smmu_pm_resume(struct device *dev)
+{
+	if (pm_suspend_target_state == PM_SUSPEND_MEM)
+		return arm_smmu_pm_restore_early(dev);
+	else
+		return arm_smmu_pm_resume_common(dev);
 }
 
 static const struct dev_pm_ops arm_smmu_pm_ops = {
