9aefe2945376402bf146153e044c73f48b0d3d42
Jagadeesh Kona <quic_jkona@quicinc.com>
2024-12-03 16:02:06 +0530

    clk: qcom: gdsc: Add support to repoll GPU CX GDSC status
    Add support to repoll GPU CX GDSC status if there is any
    failure and use aon ops for gcc gpu gemnoc clk.
    
    Change-Id: I66d78188b1a0bfaafb5c5fb47c24b7253ff4f246
    Signed-off-by: Jagadeesh Kona <quic_jkona@quicinc.com>

--- drivers/clk/qcom/gcc-sun.c
+++ drivers/clk/qcom/gcc-sun.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/clk.h>
@@ -1975,7 +1975,7 @@ static struct clk_branch gcc_gpu_gemnoc_gfx_clk = {
 		.enable_mask = BIT(0),
 		.hw.init = &(const struct clk_init_data) {
 			.name = "gcc_gpu_gemnoc_gfx_clk",
-			.ops = &clk_branch2_ops,
+			.ops = &clk_branch2_aon_ops,
 		},
 	},
 };
