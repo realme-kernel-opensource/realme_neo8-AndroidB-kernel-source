torvalds/linux

dma-heaps: Add hibernation callbacks to secure heaps
Add freeze and restore callbacks to all secure heaps. All
hyp-assigned memory which HLOS does not have access cannot
be saved during hibernation, so free all such memory back
to the system from secure pools. Abort hibernation if
clients have not freed such memory by tracking all the
allocations.

Change-Id: I8a3ba617fef5ea3fdbc60a72412af90564d75052
Signed-off-by: Vivek Kumar <quic_vivekuma@quicinc.com>
Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>
Signed-off-by: Darshankumar Jagdishchandra Thakkar <quic_djagdish@quicinc.com>

drivers/dma-buf/heaps/qcom_secure_system_heap.c
@@ -434,6 +434,7 @@ static void system_heap_free(struct qcom_sg_buffer *buffer)
 		}
 		dynamic_page_pool_free(sys_heap->pool_list[j], page);
 	}
+	atomic_long_sub(buffer->len, &sys_heap->total_allocated);
 	sg_free_table(table);
 	kfree(buffer);
 }
@@ -590,7 +591,7 @@ static struct dma_buf *system_heap_allocate(struct dma_heap *heap,
 
 	if (num_non_secure_pages)
 		sg_free_table(&non_secure_table);
-
+	atomic_long_add(len, &sys_heap->total_allocated);
 	return dmabuf;
 
 vmperm_release:
@@ -627,6 +628,46 @@ static long get_pool_size_bytes(struct dma_heap *heap)
 		total_size += dynamic_page_pool_total(sys_heap->pool_list[i], true);
 
 	return total_size << PAGE_SHIFT;
+}
+
+int qcom_secure_system_heap_freeze(void)
+{
+	struct qcom_secure_system_heap *sys_heap;
+	long sz;
+
+	/*
+	 * It is expected that the buffers are freed by the clients
+	 * before the freeze. DMABUF framework tracks the unfreed memory
+	 * by the total_allocated struct member.
+	 */
+	cancel_delayed_work_sync(&prefetch_work);
+	list_for_each_entry(sys_heap, &secure_heaps, list) {
+		sz = atomic_long_read(&sys_heap->total_allocated);
+		if (sz) {
+			pr_err("%s: %lx bytes of allocation not freed for VMID: %d. Aborting freeze.\n",
+				__func__, sz, sys_heap->vmid);
+			return -EBUSY;
+		}
+
+		dynamic_page_pool_release_pools(sys_heap->pool_list);
+	}
+	return 0;
+}
+
+int qcom_secure_system_heap_restore(void)
+{
+	struct qcom_secure_system_heap *sys_heap;
+
+	list_for_each_entry(sys_heap, &secure_heaps, list) {
+		sys_heap->pool_list = dynamic_page_pool_create_pools(sys_heap->vmid,
+					free_secure_pages);
+		if (IS_ERR(sys_heap->pool_list)) {
+			pr_err("%s: Pool creation failed for VMID: %d, err: %d\n",
+				__func__, sys_heap->vmid,
+				PTR_ERR(sys_heap->pool_list));
+		}
+	}
+	return 0;
 }
 
 static const struct dma_heap_ops system_heap_ops = {

realme-kernel-opensource/realme_neo8-AndroidB-kernel-source

(cherry picked from commit aa3be3394ddfbdbdc7d73ba574a6345c6556db0e)
Signed-off-by: Tengfei Fan <quic_tengfan@quicinc.com>
Signed-off-by: Gopikishan Thawre <gthawre@qti.qualcomm.com>

drivers/dma-buf/heaps/qcom_secure_system_heap.c
@@ -41,7 +41,7 @@
  *	Andrew F. Davis <afd@ti.com>
  *
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/dma-buf.h>
@@ -434,6 +434,7 @@ static void system_heap_free(struct qcom_sg_buffer *buffer)
 		}
 		dynamic_page_pool_free(sys_heap->pool_list[j], page);
 	}
+	atomic_long_sub(buffer->len, &sys_heap->total_allocated);
 	sg_free_table(table);
 	kfree(buffer);
 }
@@ -594,7 +595,7 @@ static struct dma_buf *system_heap_allocate(struct dma_heap *heap,
 
 	if (num_non_secure_pages)
 		sg_free_table(&non_secure_table);
-
+	atomic_long_add(len, &sys_heap->total_allocated);
 	return dmabuf;
 
 vmperm_release:
@@ -631,6 +632,46 @@ static long get_pool_size_bytes(struct dma_heap *heap)
 		total_size += dynamic_page_pool_total(sys_heap->pool_list[i], true);
 
 	return total_size << PAGE_SHIFT;
+}
+
+int qcom_secure_system_heap_freeze(void)
+{
+	struct qcom_secure_system_heap *sys_heap;
+	long sz;
+
+	/*
+	 * It is expected that the buffers are freed by the clients
+	 * before the freeze. DMABUF framework tracks the unfreed memory
+	 * by the total_allocated struct member.
+	 */
+	cancel_delayed_work_sync(&prefetch_work);
+	list_for_each_entry(sys_heap, &secure_heaps, list) {
+		sz = atomic_long_read(&sys_heap->total_allocated);
+		if (sz) {
+			pr_err("%s: %lx bytes of allocation not freed for VMID: %d. Aborting freeze.\n",
+				__func__, sz, sys_heap->vmid);
+			return -EBUSY;
+		}
+
+		dynamic_page_pool_release_pools(sys_heap->pool_list);
+	}
+	return 0;
+}
+
+int qcom_secure_system_heap_restore(void)
+{
+	struct qcom_secure_system_heap *sys_heap;
+
+	list_for_each_entry(sys_heap, &secure_heaps, list) {
+		sys_heap->pool_list = dynamic_page_pool_create_pools(sys_heap->vmid,
+					free_secure_pages);
+		if (IS_ERR(sys_heap->pool_list)) {
+			pr_err("%s: Pool creation failed for VMID: %d, err: %ld\n",
+				__func__, sys_heap->vmid,
+				PTR_ERR(sys_heap->pool_list));
+		}
+	}
+	return 0;
 }
 
 static const struct dma_heap_ops system_heap_ops = {