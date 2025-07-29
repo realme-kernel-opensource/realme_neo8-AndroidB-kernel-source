b1da750036c73ee1828858bf77a3d988d245f3ac
Aditya Chillara <quic_achillar@quicinc.com>
2025-07-29 12:37:44 +0530

    soc: qcom: minidump: Fix race in log_cpu_freq
    KCSAN reported the following race:
    
    write to 0xffffff880b7990a8 of 4 bytes by interrupt on cpu 3:
    log_cpu_freq+0x134/0x160
    __traceiter_android_vh_cpufreq_resolve_freq+0x80/0xcc
    trace_android_vh_cpufreq_resolve_freq
    __resolve_freq+0x914/0x984
    cpufreq_driver_resolve_freq+0x2c/0x3c
    get_next_freq
    waltgov_next_freq_shared+0x1384/0x187c
    waltgov_update_freq+0x5f4/0xa40
    
    read to 0xffffff880b7990a8 of 4 bytes by task 32 on cpu 1:
    log_cpu_freq+0xb8/0x160
    __traceiter_android_vh_cpufreq_resolve_freq+0x80/0xcc
    trace_android_vh_cpufreq_resolve_freq
    __resolve_freq+0x914/0x984
    cpufreq_set_policy+0x134/0xb14
    refresh_frequency_limits
    handle_update+0x78/0xec
    
    This is a valid case because multiple cpus may belong to the same cluster
    and the tracepoint hooks registered can be run on any cpu.
    
    Fix the issue by atomically incrementing the idx.
    
    Change-Id: I5e68b13674482e823d5f3abc063b015bb8bece19
    Signed-off-by: Aditya Chillara <quic_achillar@quicinc.com>

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/cache.h>
@@ -159,7 +159,7 @@ module_param_array(key_modules, charp, &n_modump, 0644);
 static bool stack_dump;
 module_param(stack_dump, bool, 0644);
 
-#define FREQ_LOG_MAX	10
+#define FREQ_LOG_IDX_MASK	0xF
 
 static int register_stack_entry(struct md_region *ksp_entry, u64 sp, u64 size)
 {
@@ -1465,8 +1465,8 @@ struct freq_log {
 };
 
 struct freq_hist {
-	uint32_t idx;
-	struct freq_log log[FREQ_LOG_MAX];
+	atomic_t idx;
+	struct freq_log log[FREQ_LOG_IDX_MASK + 1];
 };
 
 #ifdef CONFIG_QCOM_MINIDUMP_PANIC_CPUFREQ_INFO
@@ -1483,10 +1483,9 @@ static void log_cpu_freq(void *unused,
 
 	if (cluster > max_cluster)
 		return;
-	index = cpuclk_log[cluster].idx;
+	index = atomic_fetch_inc(&cpuclk_log[cluster].idx) & FREQ_LOG_IDX_MASK;
 	cpuclk_log[cluster].log[index].ktime = sched_clock();
 	cpuclk_log[cluster].log[index].freq = *target_freq;
-	cpuclk_log[cluster].idx = (index + 1) % FREQ_LOG_MAX;
 }
 
 static void register_cpufreq_log(void)
