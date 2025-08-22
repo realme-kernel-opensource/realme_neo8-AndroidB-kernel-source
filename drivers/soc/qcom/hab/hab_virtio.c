Tokito-to/kernel_xiaomi_peridot

soc: qcom: hab: add outbuf dynamic apply support
When there are many parallel processes/threads of hab clients,
the buffer in the memory pool may be insufficient. If the
non-blocking flag is set for physical_channel_send, this
physical_channel_send will fail.

In addition, if the message size sent by physical_channel_send
is larger than OUT_LARGE_BUF_SIZE, this physical_channel_send
will also fail.

In order to improve the success rate of physical_channel_send,
we will make additional attempts when get_vh_buf_header fails.

When the message sent by physical_channel_send is less than 4K,
we will use kmalloc to dynamically apply for memory.

When the message sent by physical_channel_send is larger than 4K,
we will use hab_sgl_alloc_merge to apply for memory page by page.

Change-Id: I8f96aafd1895b01d69ecced72f6d6c9cb42f35c5
Signed-off-by: Zelong Ren <quic_zeloren@quicinc.com>

drivers/soc/qcom/hab/hab_virtio.c
@@ -9,6 +9,7 @@
 #include <uapi/linux/virtio_ids.h>
 #include <linux/version.h>
 #include <linux/cma.h>
+#include <linux/scatterlist.h>
 
 #include "hab_virtio.h" /* requires hab.h */
 #include "hab_trace_os.h"
@@ -50,10 +51,14 @@ static struct virtio_device_tbl {
 enum pool_type_t {
 	PT_OUT_SMALL = 0, /* 512 bytes */
 	PT_OUT_MEDIUM,    /* 5120 bytes */
-	PT_OUT_LARGE,     /* 51200 bytes */
+	PT_OUT_LARGE,     /* 64 * 1024 bytes */
+	PT_OUT_DYNA_SGL,         /* memory allocated by hab_sgl_alloc_merge */
+	PT_OUT_DYNA_KMALLOC,     /* memory allocated by kmalloc */
 	PT_IN,          /* 5120 bytes */
 	PT_MAX
 };
+
+#define HAB_DYNAMIC_ALLOC_INDEX 0x0
 
 #define GUARD_BAND_SZ 20
 
@@ -65,6 +70,10 @@ struct vh_buf_header {
 
 	int payload_size; /* actual payload used size */
 	struct list_head node;
+
+	/* only used by PT_OUT_DYNA_SGL*/
+	struct scatterlist *sgl;
+
 	char padding[GUARD_BAND_SZ];
 };
 
@@ -74,9 +83,9 @@ struct vh_buf_header {
 #define OUT_MEDIUM_BUF_SIZE 5120
 #define OUT_LARGE_BUF_SIZE  (64 * 1024)
 
-#define IN_BUF_NUM         100 /*64*/
-#define OUT_SMALL_BUF_NUM  200 /*64*/
-#define OUT_MEDIUM_BUF_NUM 100 /*20*/
+#define IN_BUF_NUM         100
+#define OUT_SMALL_BUF_NUM  200
+#define OUT_MEDIUM_BUF_NUM 100
 #define OUT_LARGE_BUF_NUM  10
 
 #define IN_BUF_POOL_SLOT    (GUARD_BAND_SZ + \
@@ -95,6 +104,10 @@ struct vh_buf_header {
 			     sizeof(struct vh_buf_header) + \
 			     sizeof(struct hab_header) + \
 			     OUT_LARGE_BUF_SIZE)
+#define OUT_DYNAMIC_BUF_SLOT(sizebytes)  (GUARD_BAND_SZ + \
+			     sizeof(struct vh_buf_header) + \
+			     sizeof(struct hab_header) + \
+			     sizebytes)
 
 #define IN_POOL_SIZE (IN_BUF_POOL_SLOT * IN_BUF_NUM)
 #define OUT_SMALL_POOL_SIZE (OUT_SMALL_BUF_SLOT * OUT_SMALL_BUF_NUM)
@@ -129,6 +142,13 @@ static struct vq_pchan *get_virtio_pchan(struct virtio_hab *vhab,
 		return &vhab->vqpchans[index/2];
 }
 
+static inline void virtio_hab_kfree(struct vh_buf_header *hd)
+{
+	void *ptr = (void *)hd - GUARD_BAND_SZ;
+
+	kfree(ptr);
+}
+
 /* vq event callback - send/out buf returns */
 static void virthab_recv_txq(struct virtqueue *vq)
 {
@@ -151,7 +171,7 @@ static void virthab_recv_txq(struct virtqueue *vq)
 
 		while ((hd = (struct vh_buf_header *)virtqueue_get_buf(vq, &len)) != NULL) {
 			if ((hd->index < 0) || (hd->pool_type < 0) ||
-				(hd->pool_type > PT_OUT_LARGE))
+				(hd->pool_type > PT_OUT_DYNA_KMALLOC))
 				pr_err("corrupted outbuf %pK %d %d %d\n",
 					hd->buf, hd->size, hd->pool_type,
 					hd->index);
@@ -187,6 +207,23 @@ static void virthab_recv_txq(struct virtqueue *vq)
 				list_add_tail(&hd->node, &vpc->l_list);
 				vpc->l_cnt++;
 				break;
+
+			case PT_OUT_DYNA_KMALLOC:
+				if (hd->index != HAB_DYNAMIC_ALLOC_INDEX)
+					pr_err("kmem buf index corrupted %pK %pK %d %d\n",
+						hd, hd->buf, hd->index,
+						hd->size);
+				virtio_hab_kfree(hd);
+				break;
+
+			case PT_OUT_DYNA_SGL:
+				if (hd->index != HAB_DYNAMIC_ALLOC_INDEX)
+					pr_err("kmem buf index corrupted %pK %pK %d %d\n",
+						hd, hd->buf, hd->index,
+						hd->size);
+				hab_sgl_free(hd->sgl);
+				break;
+
 			default:
 				pr_err("invalid pool type %d received on txq\n",
 					hd->pool_type);
@@ -981,6 +1018,159 @@ static struct vh_buf_header *get_vh_buf_header(spinlock_t *lock,
 	return hd;
 }
 
+/**
+ * The caller needs to ensure that the memory size pointed to by pointer ptr
+ * is greater than GUARD_BAND_SZ + sizeof(struct vh_buf_header).
+ */
+static struct vh_buf_header *
+dynamic_vh_buf_header_init(void *ptr, size_t sizebytes,
+							enum pool_type_t p_type)
+{
+	struct vh_buf_header *hd;
+
+	hd = (struct vh_buf_header *)(ptr + GUARD_BAND_SZ);
+	hd->buf = ptr + GUARD_BAND_SZ + sizeof(struct vh_buf_header);
+	hd->size = sizebytes;
+	hd->pool_type = p_type;
+	/* magic number */
+	hd->index = HAB_DYNAMIC_ALLOC_INDEX;
+	hd->payload_size = 0;
+
+	return hd;
+}
+
+/* This function is only used to handle the case
+ * OUT_DYNAMIC_BUF_SLOT(payload) > PAGE_SIZE.
+ * otherwise, pls use kmalloc solution.
+ */
+static int physical_channel_send_dynamic_sgl(struct physical_channel *pchan,
+			struct hab_header *header, void *payload,
+			size_t sizebytes, gfp_t gfp)
+{
+	struct virtio_pchan_link *link =
+		(struct virtio_pchan_link *)pchan->hyp_data;
+	struct vq_pchan *vpc = link->vpc;
+	struct scatterlist *sgl;
+	struct vh_buf_header *hd = NULL;
+	unsigned int nent, nent_p = 0;
+	size_t rel_len = OUT_DYNAMIC_BUF_SLOT(sizebytes);
+	int ret = 0;
+	void *ptr = NULL;
+	size_t copy_len;
+
+	nent = round_up(rel_len, PAGE_SIZE) >> PAGE_SHIFT;
+	if (nent < 2) {
+		pr_err("payload is too small %u\n", nent);
+		return -EINVAL;
+	}
+
+	/* reduce the vring descriptor use number */
+	sgl = hab_sgl_alloc_merge(rel_len, gfp, &nent_p);
+	if (!sgl) {
+		pr_err("memory alloc failed\n");
+		return -ENOMEM;
+	}
+
+	/* use sg_virt so that we can avoid kmap(sgl_copy would use it).
+	 * assumption: only support on the 64 bits OS, since 32 bits OS
+	 * sg_virt may failed.
+	 */
+	ptr = sg_virt(sgl);
+
+	/* We will put area GUARD_BAND_SZ and area vh_buf_header
+	 *  on the first page.
+	 */
+	memset(ptr, 0, PAGE_SIZE);
+	hd = dynamic_vh_buf_header_init(ptr, sizebytes, PT_OUT_DYNA_SGL);
+	hd->payload_size = sizebytes;
+	hd->sgl = sgl;
+
+	copy_len = hab_sgl_copy_buffer(sgl, header, sizeof(*header),
+				GUARD_BAND_SZ + sizeof(struct vh_buf_header), false);
+	if (copy_len != sizeof(*header)) {
+		pr_err("copy header %d failed, copy len %d\n",
+				sizeof(*header), copy_len);
+		ret = -EIO;
+		goto err;
+	}
+
+	copy_len = hab_sgl_copy_buffer(sgl, payload, sizebytes,
+			GUARD_BAND_SZ + sizeof(struct vh_buf_header) + sizeof(*header), false);
+	if (copy_len != sizebytes) {
+		pr_err("copy error copy len %zd, expect %zd\n",
+				copy_len, sizebytes);
+		ret = -EIO;
+		goto err;
+	}
+
+	/* we only send hab_header and payload to remote */
+	sgl->offset = GUARD_BAND_SZ + sizeof(struct vh_buf_header);
+	sgl->length -= (GUARD_BAND_SZ + sizeof(struct vh_buf_header));
+
+	ret = virtqueue_add_outbuf(vpc->vq[HAB_PCHAN_TX_VQ], sgl, nent_p,
+						hd, gfp);
+	if (ret) {
+		pr_err("failed to add outbuf %d %zd bytes\n",
+			ret, sizeof(*header) + sizebytes);
+		ret = -EIO;
+		goto err;
+	}
+
+	trace_hab_pchan_send_done(pchan);
+	ret = virtqueue_kick(vpc->vq[HAB_PCHAN_TX_VQ]);
+	if (!ret) {
+		pr_err("failed to kick outbuf to PVM %d\n", ret);
+		ret = -EIO;
+		goto err;
+	}
+
+	return 0;
+
+err:
+	hab_sgl_free(sgl);
+
+	return ret;
+}
+
+static void release_msg_buffer(struct vh_buf_header *hd, struct vq_pchan *vpc)
+{
+	switch (hd->pool_type) {
+	case PT_OUT_SMALL:
+		list_add_tail(&hd->node, &vpc->s_list);
+		vpc->s_cnt++;
+		break;
+
+	case PT_OUT_MEDIUM:
+		list_add_tail(&hd->node, &vpc->m_list);
+		vpc->m_cnt++;
+		break;
+
+	case PT_OUT_LARGE:
+		list_add_tail(&hd->node, &vpc->l_list);
+		vpc->l_cnt++;
+		break;
+
+	case PT_OUT_DYNA_KMALLOC:
+		virtio_hab_kfree(hd);
+		break;
+
+	default:
+		pr_err("invalid pool type %d received on txq\n",
+			hd->pool_type);
+	}
+}
+
+static struct vh_buf_header *kmalloc_get_vh_buf_header(size_t sizebytes)
+{
+	void *ptr = NULL;
+
+	ptr = kzalloc(OUT_DYNAMIC_BUF_SLOT(sizebytes), GFP_ATOMIC);
+	if (!ptr)
+		return ERR_PTR(-ENOMEM);
+
+	return dynamic_vh_buf_header_init(ptr, sizebytes, PT_OUT_DYNA_KMALLOC);
+}
+
 int physical_channel_send(struct physical_channel *pchan,
 			struct hab_header *header, void *payload,
 			unsigned int flags)
@@ -991,7 +1181,7 @@ int physical_channel_send(struct physical_channel *pchan,
 	struct vq_pchan *vpc = link->vpc;
 	struct scatterlist sgout[1];
 	char *outbuf = NULL;
-	int rc;
+	int rc = 0;
 	unsigned long lock_flags;
 	struct vh_buf_header *hd = NULL;
 	int nonblocking_flag = flags & HABMM_SOCKET_SEND_FLAGS_NON_BLOCKING;
@@ -999,13 +1189,6 @@ int physical_channel_send(struct physical_channel *pchan,
 	if (link->vpc == NULL) {
 		pr_err("%s: %s link->vpc not ready\n", __func__, pchan->name);
 		return -ENODEV;
-	}
-
-	if (sizebytes > OUT_LARGE_BUF_SIZE) {
-		pr_err("send size %zd overflow %d available %d %d %d\n",
-			   sizebytes, OUT_LARGE_BUF_SIZE,
-			   vpc->s_cnt, vpc->m_cnt, vpc->l_cnt);
-		return -EINVAL;
 	}
 
 	trace_hab_pchan_send_start(pchan);
@@ -1023,17 +1206,11 @@ int physical_channel_send(struct physical_channel *pchan,
 						&lock_flags, &vpc->m_list,
 						&vpc->out_wq, &vpc->m_cnt,
 						nonblocking_flag);
-		} else {
+		} else if (sizebytes <= OUT_LARGE_BUF_SIZE) {
 			hd = get_vh_buf_header(&vpc->lock[HAB_PCHAN_TX_VQ],
 						&lock_flags, &vpc->l_list,
 						&vpc->out_wq, &vpc->l_cnt,
 						nonblocking_flag);
-		}
-
-		if (IS_ERR(hd) && nonblocking_flag) {
-			spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ], lock_flags);
-			pr_info("get_vh_buf_header failed in non-blocking mode\n");
-			return PTR_ERR(hd);
 		}
 
 		if (HAB_HEADER_GET_TYPE(*header) == HAB_PAYLOAD_TYPE_PROFILE) {
@@ -1049,6 +1226,41 @@ int physical_channel_send(struct physical_channel *pchan,
 		header->sequence = ++pchan->sequence_tx;
 		header->signature = HAB_HEAD_SIGNATURE;
 
+		if (IS_ERR_OR_NULL(hd)) { /* let's try again */
+		/* We do not use kmalloc to try to apply for memory larger than a page_size. */
+			if (OUT_DYNAMIC_BUF_SLOT(sizebytes) <= PAGE_SIZE) {
+				hd = kmalloc_get_vh_buf_header(sizebytes);
+				if (IS_ERR_OR_NULL(hd)) {
+					spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ],
+											lock_flags);
+					pr_info("get_vh_buf_header failed, size %d, non-block flag %d\n",
+							sizebytes, nonblocking_flag);
+					return PTR_ERR(hd);
+				}
+			} else {
+				/**
+				 * When we reach the current branch, there are two cases
+				 * 1. Call physical_channel_send with non-blocking flag and the
+				 * memory required to send the message is larger than a page.
+				 * 2. The memory required to send the message is over 64k.
+				 *
+				 * For the above two cases, the success rate of apply memory page
+				 * by page is higher than apply continuous memory using kmalloc.
+				 * Although the efficiency of memory application is reduced
+				 * when using hab_sgl_alloc_merge, the success rate will be
+				 * greatly improved. This is what we expect.
+				 */
+				rc = physical_channel_send_dynamic_sgl(pchan, header,
+							payload, sizebytes, GFP_ATOMIC);
+				spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ], lock_flags);
+				return rc;
+			}
+		}
+
+		/**
+		 * Single outbuf path, using a buffer allocated from static memory pool
+		 * or kmalloc
+		 */
 		outbuf = hd->buf;
 		hd->payload_size = sizebytes;
 
@@ -1062,18 +1274,29 @@ int physical_channel_send(struct physical_channel *pchan,
 		if (!rc) {
 			trace_hab_pchan_send_done(pchan);
 			rc = virtqueue_kick(vpc->vq[HAB_PCHAN_TX_VQ]);
-			if (!rc)
+			if (!rc) {
+				/**
+				 * Return to caller.
+				 * We cannot release outbuf because it may be used by the
+				 * remote side (even if kick fails). We must release outbuf
+				 * safely in function virthab_recv_txq.
+				 */
 				pr_err("failed to kick outbuf to PVM %d\n", rc);
-		} else
+				rc = -EIO;
+			} else
+				rc = 0;
+		} else {
 			pr_err("failed to add outbuf %d %zd bytes\n",
 				rc, sizeof(*header) + sizebytes);
+			release_msg_buffer(hd, vpc);
+		}
 	} else {
 		pr_err("%s pchan not ready\n", pchan->name);
 		rc = -ENODEV;
 	}
 	spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ], lock_flags);
 
-	return 0;
+	return rc;
 }
 
 /* this read is called by hab-msg-recv from physical_channel_rx_dispatch or cb */

realme-kernel-opensource/realme_neo8-AndroidB-kernel-source

(cherry picked from commit d5d72c250b7e4c560f27fd927b665d165df69da5)

drivers/soc/qcom/hab/hab_virtio.c
@@ -9,6 +9,7 @@
 #include <uapi/linux/virtio_ids.h>
 #include <linux/version.h>
 #include <linux/cma.h>
+#include <linux/scatterlist.h>
 
 #include "hab_virtio.h" /* requires hab.h */
 #include "hab_trace_os.h"
@@ -51,10 +52,14 @@ static struct virtio_device_tbl {
 enum pool_type_t {
 	PT_OUT_SMALL = 0, /* 512 bytes */
 	PT_OUT_MEDIUM,    /* 5120 bytes */
-	PT_OUT_LARGE,     /* 51200 bytes */
+	PT_OUT_LARGE,     /* 64 * 1024 bytes */
+	PT_OUT_DYNA_SGL,         /* memory allocated by hab_sgl_alloc_merge */
+	PT_OUT_DYNA_KMALLOC,     /* memory allocated by kmalloc */
 	PT_IN,          /* 5120 bytes */
 	PT_MAX
 };
+
+#define HAB_DYNAMIC_ALLOC_INDEX 0x0
 
 #define GUARD_BAND_SZ 20
 
@@ -66,6 +71,10 @@ struct vh_buf_header {
 
 	int payload_size; /* actual payload used size */
 	struct list_head node;
+
+	/* only used by PT_OUT_DYNA_SGL*/
+	struct scatterlist *sgl;
+
 	char padding[GUARD_BAND_SZ];
 };
 
@@ -75,9 +84,9 @@ struct vh_buf_header {
 #define OUT_MEDIUM_BUF_SIZE 5120
 #define OUT_LARGE_BUF_SIZE  (64 * 1024)
 
-#define IN_BUF_NUM         100 /*64*/
-#define OUT_SMALL_BUF_NUM  200 /*64*/
-#define OUT_MEDIUM_BUF_NUM 100 /*20*/
+#define IN_BUF_NUM         100
+#define OUT_SMALL_BUF_NUM  200
+#define OUT_MEDIUM_BUF_NUM 100
 #define OUT_LARGE_BUF_NUM  10
 
 #define IN_BUF_POOL_SLOT    (GUARD_BAND_SZ + \
@@ -96,6 +105,10 @@ struct vh_buf_header {
 			     sizeof(struct vh_buf_header) + \
 			     sizeof(struct hab_header) + \
 			     OUT_LARGE_BUF_SIZE)
+#define OUT_DYNAMIC_BUF_SLOT(sizebytes)  (GUARD_BAND_SZ + \
+			     sizeof(struct vh_buf_header) + \
+			     sizeof(struct hab_header) + \
+			     sizebytes)
 
 #define IN_POOL_SIZE (IN_BUF_POOL_SLOT * IN_BUF_NUM)
 #define OUT_SMALL_POOL_SIZE (OUT_SMALL_BUF_SLOT * OUT_SMALL_BUF_NUM)
@@ -148,6 +161,13 @@ static struct vq_pchan *get_virtio_pchan(struct virtio_hab *vhab,
 		return &vhab->vqpchans[index/2];
 }
 
+static inline void virtio_hab_kfree(struct vh_buf_header *hd)
+{
+	void *ptr = (void *)hd - GUARD_BAND_SZ;
+
+	kfree(ptr);
+}
+
 /* vq event callback - send/out buf returns */
 static void virthab_recv_txq(struct virtqueue *vq)
 {
@@ -170,7 +190,7 @@ static void virthab_recv_txq(struct virtqueue *vq)
 
 		while ((hd = (struct vh_buf_header *)virtqueue_get_buf(vq, &len)) != NULL) {
 			if ((hd->index < 0) || (hd->pool_type < 0) ||
-				(hd->pool_type > PT_OUT_LARGE))
+				(hd->pool_type > PT_OUT_DYNA_KMALLOC))
 				pr_err("corrupted outbuf %pK %d %d %d\n",
 					hd->buf, hd->size, hd->pool_type,
 					hd->index);
@@ -206,6 +226,23 @@ static void virthab_recv_txq(struct virtqueue *vq)
 				list_add_tail(&hd->node, &vpc->l_list);
 				vpc->l_cnt++;
 				break;
+
+			case PT_OUT_DYNA_KMALLOC:
+				if (hd->index != HAB_DYNAMIC_ALLOC_INDEX)
+					pr_err("kmem buf index corrupted %pK %pK %d %d\n",
+						hd, hd->buf, hd->index,
+						hd->size);
+				virtio_hab_kfree(hd);
+				break;
+
+			case PT_OUT_DYNA_SGL:
+				if (hd->index != HAB_DYNAMIC_ALLOC_INDEX)
+					pr_err("kmem buf index corrupted %pK %pK %d %d\n",
+						hd, hd->buf, hd->index,
+						hd->size);
+				hab_sgl_free(hd->sgl);
+				break;
+
 			default:
 				pr_err("invalid pool type %d received on txq\n",
 					hd->pool_type);
@@ -1021,6 +1058,159 @@ static struct vh_buf_header *get_vh_buf_header(spinlock_t *lock,
 	return hd;
 }
 
+/**
+ * The caller needs to ensure that the memory size pointed to by pointer ptr
+ * is greater than GUARD_BAND_SZ + sizeof(struct vh_buf_header).
+ */
+static struct vh_buf_header *
+dynamic_vh_buf_header_init(void *ptr, size_t sizebytes,
+							enum pool_type_t p_type)
+{
+	struct vh_buf_header *hd;
+
+	hd = (struct vh_buf_header *)(ptr + GUARD_BAND_SZ);
+	hd->buf = ptr + GUARD_BAND_SZ + sizeof(struct vh_buf_header);
+	hd->size = sizebytes;
+	hd->pool_type = p_type;
+	/* magic number */
+	hd->index = HAB_DYNAMIC_ALLOC_INDEX;
+	hd->payload_size = 0;
+
+	return hd;
+}
+
+/* This function is only used to handle the case
+ * OUT_DYNAMIC_BUF_SLOT(payload) > PAGE_SIZE.
+ * otherwise, pls use kmalloc solution.
+ */
+static int physical_channel_send_dynamic_sgl(struct physical_channel *pchan,
+			struct hab_header *header, void *payload,
+			size_t sizebytes, gfp_t gfp)
+{
+	struct virtio_pchan_link *link =
+		(struct virtio_pchan_link *)pchan->hyp_data;
+	struct vq_pchan *vpc = link->vpc;
+	struct scatterlist *sgl;
+	struct vh_buf_header *hd = NULL;
+	unsigned int nent, nent_p = 0;
+	size_t rel_len = OUT_DYNAMIC_BUF_SLOT(sizebytes);
+	int ret = 0;
+	void *ptr = NULL;
+	size_t copy_len;
+
+	nent = round_up(rel_len, PAGE_SIZE) >> PAGE_SHIFT;
+	if (nent < 2) {
+		pr_err("payload is too small %u\n", nent);
+		return -EINVAL;
+	}
+
+	/* reduce the vring descriptor use number */
+	sgl = hab_sgl_alloc_merge(rel_len, gfp, &nent_p);
+	if (!sgl) {
+		pr_err("memory alloc failed\n");
+		return -ENOMEM;
+	}
+
+	/* use sg_virt so that we can avoid kmap(sgl_copy would use it).
+	 * assumption: only support on the 64 bits OS, since 32 bits OS
+	 * sg_virt may failed.
+	 */
+	ptr = sg_virt(sgl);
+
+	/* We will put area GUARD_BAND_SZ and area vh_buf_header
+	 *  on the first page.
+	 */
+	memset(ptr, 0, PAGE_SIZE);
+	hd = dynamic_vh_buf_header_init(ptr, sizebytes, PT_OUT_DYNA_SGL);
+	hd->payload_size = sizebytes;
+	hd->sgl = sgl;
+
+	copy_len = hab_sgl_copy_buffer(sgl, header, sizeof(*header),
+				GUARD_BAND_SZ + sizeof(struct vh_buf_header), false);
+	if (copy_len != sizeof(*header)) {
+		pr_err("copy header %lu failed, copy len %zu\n",
+				sizeof(*header), copy_len);
+		ret = -EIO;
+		goto err;
+	}
+
+	copy_len = hab_sgl_copy_buffer(sgl, payload, sizebytes,
+			GUARD_BAND_SZ + sizeof(struct vh_buf_header) + sizeof(*header), false);
+	if (copy_len != sizebytes) {
+		pr_err("copy error copy len %zd, expect %zd\n",
+				copy_len, sizebytes);
+		ret = -EIO;
+		goto err;
+	}
+
+	/* we only send hab_header and payload to remote */
+	sgl->offset = GUARD_BAND_SZ + sizeof(struct vh_buf_header);
+	sgl->length -= (GUARD_BAND_SZ + sizeof(struct vh_buf_header));
+
+	ret = virtqueue_add_outbuf(vpc->vq[HAB_PCHAN_TX_VQ], sgl, nent_p,
+						hd, gfp);
+	if (ret) {
+		pr_err("failed to add outbuf %d %zd bytes\n",
+			ret, sizeof(*header) + sizebytes);
+		ret = -EIO;
+		goto err;
+	}
+
+	trace_hab_pchan_send_done(pchan);
+	ret = virtqueue_kick(vpc->vq[HAB_PCHAN_TX_VQ]);
+	if (!ret) {
+		pr_err("failed to kick outbuf to PVM %d\n", ret);
+		ret = -EIO;
+		goto err;
+	}
+
+	return 0;
+
+err:
+	hab_sgl_free(sgl);
+
+	return ret;
+}
+
+static void release_msg_buffer(struct vh_buf_header *hd, struct vq_pchan *vpc)
+{
+	switch (hd->pool_type) {
+	case PT_OUT_SMALL:
+		list_add_tail(&hd->node, &vpc->s_list);
+		vpc->s_cnt++;
+		break;
+
+	case PT_OUT_MEDIUM:
+		list_add_tail(&hd->node, &vpc->m_list);
+		vpc->m_cnt++;
+		break;
+
+	case PT_OUT_LARGE:
+		list_add_tail(&hd->node, &vpc->l_list);
+		vpc->l_cnt++;
+		break;
+
+	case PT_OUT_DYNA_KMALLOC:
+		virtio_hab_kfree(hd);
+		break;
+
+	default:
+		pr_err("invalid pool type %d received on txq\n",
+			hd->pool_type);
+	}
+}
+
+static struct vh_buf_header *kmalloc_get_vh_buf_header(size_t sizebytes)
+{
+	void *ptr = NULL;
+
+	ptr = kzalloc(OUT_DYNAMIC_BUF_SLOT(sizebytes), GFP_ATOMIC);
+	if (!ptr)
+		return ERR_PTR(-ENOMEM);
+
+	return dynamic_vh_buf_header_init(ptr, sizebytes, PT_OUT_DYNA_KMALLOC);
+}
+
 int virtio_physical_channel_send(struct physical_channel *pchan,
 			struct hab_header *header, void *payload,
 			unsigned int flags)
@@ -1031,7 +1221,7 @@ int virtio_physical_channel_send(struct physical_channel *pchan,
 	struct vq_pchan *vpc = link->vpc;
 	struct scatterlist sgout[1];
 	char *outbuf = NULL;
-	int rc;
+	int rc = 0;
 	unsigned long lock_flags;
 	struct vh_buf_header *hd = NULL;
 	int nonblocking_flag = flags & HABMM_SOCKET_SEND_FLAGS_NON_BLOCKING;
@@ -1039,13 +1229,6 @@ int virtio_physical_channel_send(struct physical_channel *pchan,
 	if (link->vpc == NULL) {
 		pr_err("%s: %s link->vpc not ready\n", __func__, pchan->name);
 		return -ENODEV;
-	}
-
-	if (sizebytes > OUT_LARGE_BUF_SIZE) {
-		pr_err("send size %zd overflow %d available %d %d %d\n",
-			   sizebytes, OUT_LARGE_BUF_SIZE,
-			   vpc->s_cnt, vpc->m_cnt, vpc->l_cnt);
-		return -EINVAL;
 	}
 
 	trace_hab_pchan_send_start(pchan);
@@ -1063,17 +1246,11 @@ int virtio_physical_channel_send(struct physical_channel *pchan,
 						&lock_flags, &vpc->m_list,
 						&vpc->out_wq, &vpc->m_cnt,
 						nonblocking_flag);
-		} else {
+		} else if (sizebytes <= OUT_LARGE_BUF_SIZE) {
 			hd = get_vh_buf_header(&vpc->lock[HAB_PCHAN_TX_VQ],
 						&lock_flags, &vpc->l_list,
 						&vpc->out_wq, &vpc->l_cnt,
 						nonblocking_flag);
-		}
-
-		if (IS_ERR(hd) && nonblocking_flag) {
-			spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ], lock_flags);
-			pr_info("get_vh_buf_header failed in non-blocking mode\n");
-			return PTR_ERR(hd);
 		}
 
 		if (HAB_HEADER_GET_TYPE(*header) == HAB_PAYLOAD_TYPE_PROFILE) {
@@ -1089,6 +1266,41 @@ int virtio_physical_channel_send(struct physical_channel *pchan,
 		header->sequence = ++pchan->sequence_tx;
 		header->signature = HAB_HEAD_SIGNATURE;
 
+		if (IS_ERR_OR_NULL(hd)) { /* let's try again */
+		/* We do not use kmalloc to try to apply for memory larger than a page_size. */
+			if (OUT_DYNAMIC_BUF_SLOT(sizebytes) <= PAGE_SIZE) {
+				hd = kmalloc_get_vh_buf_header(sizebytes);
+				if (IS_ERR_OR_NULL(hd)) {
+					spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ],
+											lock_flags);
+					pr_info("get_vh_buf_header failed, size %zu, non-block flag %d\n",
+							sizebytes, nonblocking_flag);
+					return PTR_ERR(hd);
+				}
+			} else {
+				/**
+				 * When we reach the current branch, there are two cases
+				 * 1. Call physical_channel_send with non-blocking flag and the
+				 * memory required to send the message is larger than a page.
+				 * 2. The memory required to send the message is over 64k.
+				 *
+				 * For the above two cases, the success rate of apply memory page
+				 * by page is higher than apply continuous memory using kmalloc.
+				 * Although the efficiency of memory application is reduced
+				 * when using hab_sgl_alloc_merge, the success rate will be
+				 * greatly improved. This is what we expect.
+				 */
+				rc = physical_channel_send_dynamic_sgl(pchan, header,
+							payload, sizebytes, GFP_ATOMIC);
+				spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ], lock_flags);
+				return rc;
+			}
+		}
+
+		/**
+		 * Single outbuf path, using a buffer allocated from static memory pool
+		 * or kmalloc
+		 */
 		outbuf = hd->buf;
 		hd->payload_size = sizebytes;
 
@@ -1102,18 +1314,29 @@ int virtio_physical_channel_send(struct physical_channel *pchan,
 		if (!rc) {
 			trace_hab_pchan_send_done(pchan);
 			rc = virtqueue_kick(vpc->vq[HAB_PCHAN_TX_VQ]);
-			if (!rc)
+			if (!rc) {
+				/**
+				 * Return to caller.
+				 * We cannot release outbuf because it may be used by the
+				 * remote side (even if kick fails). We must release outbuf
+				 * safely in function virthab_recv_txq.
+				 */
 				pr_err("failed to kick outbuf to PVM %d\n", rc);
-		} else
+				rc = -EIO;
+			} else
+				rc = 0;
+		} else {
 			pr_err("failed to add outbuf %d %zd bytes\n",
 				rc, sizeof(*header) + sizebytes);
+			release_msg_buffer(hd, vpc);
+		}
 	} else {
 		pr_err("%s pchan not ready\n", pchan->name);
 		rc = -ENODEV;
 	}
 	spin_unlock_irqrestore(&vpc->lock[HAB_PCHAN_TX_VQ], lock_flags);
 
-	return 0;
+	return rc;
 }
 
 /* this read is called by hab-msg-recv from physical_channel_rx_dispatch or cb */